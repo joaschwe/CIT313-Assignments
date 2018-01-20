d179f0d9248e994bc68fcb270235adc9d16675f9
joaschwe <joaschwe@iupui.edu>
2018-01-20 00:40:53 -0500

    Add files via upload
    Uploaded assignment files

---
+++ ex1/_includes/config.php
@@ -0,0 +1,5 @@
+<?php
+	define("ABSOLUTE_PATH", "/home/joaschwe/htdocs/CIT313/SP2018/ex1");
+
+	define("URL_ROOT", "http://corsair.cs.iupui.edu:21791/CIT313/SP2018/ex1");
+?>
---
+++ ex1/_includes/header.inc.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Schweiger EX1:PHP Review</title>
+    <style>
+        body { margin: 0; padding: 0; color: #333; font: 1em/1.2 "Helvetica Neue", Helvetica, Arial, Geneva, sans-serif; }
+        h1 { margin: 0 0; font-size: 2.25em; /* 36px */ }
+        hr { height: 0; margin-bottom: 1em; border: 0; border-top: 1px solid #ddd; }
+        .container { max-width: 50em; margin: 0 auto; }
+        .content { padding: 1em 1.25em; }
+        .foot { padding: 0.8em 1.25em; }
+    </style>
+</head>
