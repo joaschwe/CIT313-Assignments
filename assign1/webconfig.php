a663692050d6355876b03f13285631e8be820944
joaschwe <joaschwe@iupui.edu>
2018-01-28 13:50:10 -0500

    Added files via upload

---
+++ assign1/webconfig.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/joaschwe/htdocs/CIT313/SP2018/a1--phpDebug/phpDebug-v2');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/a1--phpDebug/phpDebug-v2');
