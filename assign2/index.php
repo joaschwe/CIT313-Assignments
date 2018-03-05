6e9f86ded4d2115c8a47882393b40d3b7871d126
joaschwe <joaschwe@iupui.edu>
2018-03-05 11:07:00 -0500

    Add files via upload

---
+++ assign2/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joanna_schweiger
+ * Date: 2/25/18
+ * Time: 7:11 PM
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require 'application/router.php';
