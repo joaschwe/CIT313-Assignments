2055ef7b7291f5ab30ecde918482fa9e69f30080
joaschwe <joaschwe@iupui.edu>
2018-03-05 11:06:42 -0500

    Add files via upload

---
+++ assign2/application/router.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joanna_schweiger
+ * Date: 2/25/18
+ * Time: 7:11 PM
+ */
+
+include_once 'controllers/controller.class.php';
+include_once 'load.class.php';
+include_once 'models/model.class.php';
+
+//include_once '../application/controllers/controller.class.php';
+//include_once '../application/models/model.class.php';
+
+//code here for file_exists()
+
+new controller();
