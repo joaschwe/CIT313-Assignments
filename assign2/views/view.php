1b1598a49dcda654ad55cd141ea7126c7a4ca12e
joaschwe <joaschwe@iupui.edu>
2018-03-05 11:09:03 -0500

    Add files via upload

---
+++ assign2/views/view.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joanna_schweiger
+ * Date: 2/25/18
+ * Time: 7:11 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Schweiger-Model View Controller</title>
+</head>
+
+<body>
+    <h1>Hello from my view!</h1>
+
+<?php
+    echo 'UserID: ' . $userID . '<br>';
+    echo 'First Name: ' . $first . '<br>';
+    echo 'Last Name: ' . $last . '<br>';
+    echo 'Email: ' . $email . '<br>';
+    echo 'Role: ' . $role . '<br>';
+
+
+?>
+
+</body>
+
+</html>
