0f0351522e09211412e34490cea37618e2a68100
joaschwe <joaschwe@iupui.edu>
2018-03-07 01:26:54 -0500

    uploaded file

--- assign2/application/controllers/controller.class.php
+++ assign2/application/controllers/controller.class.php
@@ -1,14 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joanna_schweiger
- * Date: 2/25/18
- * Time: 7:11 PM
- */
-
 class controller {
+
     public $load;
     public $user;
+
+
+    function __construct() {
+        $this->load = new Load();
+        $this->user = new User();
+        $this->home();
+    }
 
     function home() {
         $this->user->userID = 'joaschwe';
@@ -17,27 +18,8 @@ class controller {
         $this->user->email = 'joaschwe@iupui.edu';
         $this->user->role = 'admin';
 
+
         $data = $this->user->getName();
         $this->load->view('view.php',$data);
     }
-
-    function __construct() {
-        $this -> load = new load();
-        $this -> user = new user();
-        $this -> home();
-    }
-
-    public function __set($name, $value)
-    {
-        $this->$name = $value;
-    }
-
-    public function __get($name)
-    {
-        return $this->$name;
-    }
-
-    public function __destruct() { }
-
-
 }
