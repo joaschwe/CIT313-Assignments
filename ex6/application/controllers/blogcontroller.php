2ad50d47be762cefa2b997a235fc714e0a446ca9
joaschwe <joaschwe@iupui.edu>
2018-04-08 18:45:12 -0400

    changed method view to post; defaultTask to index

--- ex6/application/controllers/blogcontroller.php
+++ ex6/application/controllers/blogcontroller.php
@@ -3,13 +3,13 @@ class BlogController extends Controller{
 	
 	public $postObject;
   
-   	public function view($pID){
+   	public function post($pID){
 		$this->postObject = new Post();
 		$post = $this->postObject->getPost($pID);	    
 	  	$this->set('post',$post);
    	}
 	
-	public function defaultTask(){
+	public function index(){
 		$this->postObject = new Post();
 		$posts = $this->postObject->getAllPosts();
 		$this->set('title', 'The Default Blog View');
