ea73432cece1ed1bb25e8f8a01025db8fff7414f
joaschwe <joaschwe@iupui.edu>
2018-04-08 17:00:21 -0400

    changed path to ex6

--- ex6/application/config.php
+++ ex6/application/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
-define('BASE_URL', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/ex6/');
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/ex5/');
 
 //database info
 define('DB_HOST', 'localhost');
