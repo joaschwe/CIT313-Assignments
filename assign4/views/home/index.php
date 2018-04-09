69ae04aa52ed270eae461a99dbca2b1f031c2a35
joaschwe <joaschwe@iupui.edu>
2018-04-08 20:24:02 -0400

    uploading copy of ex6 files to use as start for base files

---
+++ assign4/views/home/index.php
@@ -0,0 +1,20 @@
+<?php include('views/elements/header.php'); ?>
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Hello From the View</h1>
+        </div>
+
+        <p>
+            <?php
+            if (isset($_SESSION['uID'])) {
+                echo '<p>User ID number ' . $_SESSION['uID'] . ', ' . $_SESSION['last_name'] . ', is logged in.</p>';
+            } else {
+                echo '<p>No one is logged in.</p>';
+            }
+            ?>
+        </p>
+
+    </div>
+
+<?php include('views/elements/footer.php'); ?>
