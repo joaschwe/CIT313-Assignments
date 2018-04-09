2452e0eb654b5487abc305051eb796c429275b52
joaschwe <joaschwe@iupui.edu>
2018-04-08 22:19:01 -0400

    setting up rss feed object

--- assign4/application/controllers/homecontroller.php
+++ assign4/application/controllers/homecontroller.php
@@ -2,6 +2,20 @@
 class HomeController extends Controller{
 
 	public function index(){
+	    $feed = 'http://fox59.com/feed';
+
+	    //instantiate object
+	    $rss = new RssDisplay($feed);
+
+	    $feed_data = $rss->getFeedItems();
+
+//	    var_dump($feed_data);
+
+        $channel_title = $feed_data->channel->title;
+        $this->set('rss_title', $channel_title);
+
+        $article_title = $feed_data->channel->item->title;
+        $this->set('rss_article_descrip', $article_title);
 
 	}
 	
