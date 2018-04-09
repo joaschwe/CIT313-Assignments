4d5d6f2b6c9db430a4f3d97a5927a0f6a4690dc5
joaschwe <joaschwe@iupui.edu>
2018-04-09 01:50:21 -0400

    added form to input zip for weather lookup

--- assign4/views/weather/index.php
+++ assign4/views/weather/index.php
@@ -1,5 +1,21 @@
 <?php include('views/elements/header.php'); ?>
 
+    <div class="container" id="wx">
+        <div class="page-header">
+            <h1>Weather</h1>
+        </div>
+
+        <form method="post" action="<?php echo BASE_URL; ?>weather/getResults()">
+            <label for="zip">Enter Your Zipcode</label>
+            <input type="text" name="zip" id="zip" required="zip" />
+            <br/>
+            <button type="submit" class="btn btn-primary">Get Weather</button>
+
+        </form>
+
+    </div>
+
+
     <div class="container">
         <div class="page-header">
             <h1>Weather for <?php echo $weather->request->query; ?></h1>
