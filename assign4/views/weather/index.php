4d33d3bde4ce9cd435160979960b274d1abed4ec
joaschwe <joaschwe@iupui.edu>
2018-04-09 03:23:12 -0400

    back to world wide weather

--- assign4/views/weather/index.php
+++ assign4/views/weather/index.php
@@ -22,7 +22,7 @@ else {  ?>
 
     <div class="container">
         <div class="page-header">
-            <h1>Current Weather for <?php echo $weather->current_observation->display_location->full; ?></h1>
+            <h1>Current Weather for <?php echo $weather->request->query; ?></h1>
         </div>
 
         <h4>Today's High: <?php echo $weather->weather->maxtempF; ?></h4>
