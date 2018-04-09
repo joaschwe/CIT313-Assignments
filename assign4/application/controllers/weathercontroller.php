353123abf5907eca7b97e6ceaeed6ffc652e4636
joaschwe <joaschwe@iupui.edu>
2018-04-09 01:49:59 -0400

    deleted old key

--- assign4/application/controllers/weathercontroller.php
+++ assign4/application/controllers/weathercontroller.php
@@ -1,8 +1,7 @@
 <?php
 class WeatherController extends Controller{
 
-	public function index(){
-//	    $xml = simplexml_load_file('api.worldweatheronline.free/v2/weather.ashx?q=46076&format=xml&num_of_days=2&key=7360a0596f8f4826b1845340180904');
+	public function index() {
 	    $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q=46076&format=xml&num_of_days=2');
 	    $this->set(weather, $xml);
 
