27b84d2d94576663db34b5ef5550bdc12e0da42a
joaschwe <joaschwe@iupui.edu>
2018-04-08 22:19:41 -0400

    created model to work with rss feed

---
+++ assign4/application/models/rssdisplay.php
@@ -0,0 +1,23 @@
+<?php
+
+class RssDisplay extends Model {
+
+    protected $feed_url;
+    protected $num_items;
+
+    public function __construct($url) {
+        parent::__construct();
+
+        $this->feed_url = $url;
+    }
+
+    public function getFeedItems() {
+        $items = simplexml_load_file($this->feed_url);
+//        return $items;
+        $array = (array)$items;
+        return $array;
+
+//        $xml = simplexml_load_string($file);
+//        $array = (array)$xml;
+    }
+}
