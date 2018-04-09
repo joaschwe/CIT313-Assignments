3ca0888e228c6b2a5813ed7a14ee270ba57fe807
joaschwe <joaschwe@iupui.edu>
2018-04-09 00:45:51 -0400

    trying to get $items to load in home

--- assign4/application/models/rssdisplay.php
+++ assign4/application/models/rssdisplay.php
@@ -11,13 +11,34 @@ class RssDisplay extends Model {
         $this->feed_url = $url;
     }
 
-    public function getFeedItems() {
+//    public function getFeedItems() {
+    public function getFeedItems($num_items = 8) {
+
+//        if($num_items < 8){
+//            $somethingB = ' LIMIT '.$num_items;
+//        }
+//
+//        $somethingA = $somethingB;
+
+
+
+
         $items = simplexml_load_file($this->feed_url);
         return $items;
 //        $array = (array)$items;
+//        $array = array($items);
+//        echo $array;
 //        return $array;
 
-//        $xml = simplexml_load_string($file);
-//        $array = (array)$xml;
+//        ^^ example: $xml = simplexml_load_string($file);
+//                 $array = (array)$xml;
+
+
+//        $arrlength = count($array);
+
+//        for ($x = 0; $x < $num_items; $x++) {
+//            array[$array] = $items;
+//            echo $array[$x];
+//        }
     }
 }
