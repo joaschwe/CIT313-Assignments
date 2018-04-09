b952b6e6ac8b82531c16f9fcc7f2ae58f4928fee
joaschwe <joaschwe@iupui.edu>
2018-04-08 22:32:29 -0400

    removed user info, added pubDate

--- assign4/views/home/index.php
+++ assign4/views/home/index.php
@@ -3,28 +3,15 @@
     <div class="container">
         <div class="page-header">
             <h1><?php echo $rss_title ?></h1>
-
-            <p>
-                <?php
-                if (isset($_SESSION['uID'])) {
-                    echo '<p>User ID number ' . $_SESSION['uID'] . ', ' . $_SESSION['last_name'] . ', is logged in.</p>';
-                } else {
-                    echo '<p>No one is logged in.</p>';
-                }
-                ?>
-            </p>
         </div>
-
-
 
         <p>
             <?php
             $rss = simplexml_load_file('http://www.fox59.com/feed');
 //            var_dump($rss);
 
-            echo '<h1>' . $rss->channel->title . '</h1>';
             foreach ($rss->channel->item as $item) {
-                echo $item->title.'<br/>'.$item->link.'<br/>'.$item->comments->pubDate.'<hr>';
+                echo $item->title.'<br/>'.$item->link.'<br/>'.$item->pubDate.'<hr>';
             }
 
             ?>
