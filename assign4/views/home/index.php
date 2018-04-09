373b1db0231d56716f82f9598f3d391d9a7fa2ff
joaschwe <joaschwe@iupui.edu>
2018-04-08 21:14:31 -0400

    experimenting with RSS feed output

--- assign4/views/home/index.php
+++ assign4/views/home/index.php
@@ -3,24 +3,27 @@
     <div class="container">
         <div class="page-header">
             <h1>Hello From the View</h1>
+
+            <p>
+                <?php
+                if (isset($_SESSION['uID'])) {
+                    echo '<p>User ID number ' . $_SESSION['uID'] . ', ' . $_SESSION['last_name'] . ', is logged in.</p>';
+                } else {
+                    echo '<p>No one is logged in.</p>';
+                }
+                ?>
+            </p>
         </div>
 
-        <p>
-            <?php
-            if (isset($_SESSION['uID'])) {
-                echo '<p>User ID number ' . $_SESSION['uID'] . ', ' . $_SESSION['last_name'] . ', is logged in.</p>';
-            } else {
-                echo '<p>No one is logged in.</p>';
-            }
-            ?>
-        </p>
+
 
         <p>
             <?php
             $rss = simplexml_load_file('http://www.fox59.com/feed');
-            var_dump($rss);
-
-
+//            var_dump($rss);
+            foreach ($rss->channel->item as $item) {
+                echo $item->title.'<br/>'.$item->link.'<hr>';
+            }
 
             ?>
 
