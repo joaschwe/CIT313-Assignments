5ad56550d28d20745c5998d71ca435d915739732
joaschwe <joaschwe@iupui.edu>
2018-04-09 01:40:32 -0400

    added current max and min temp

--- assign4/views/weather/index.php
+++ assign4/views/weather/index.php
@@ -4,6 +4,9 @@
         <div class="page-header">
             <h1>Weather for <?php echo $weather->request->query; ?></h1>
         </div>
+
+        <h4>Today's High: <?php echo $weather->weather->maxtempF; ?></h4>
+        <h4>Today's Low: <?php echo $weather->weather->mintempF; ?></h4>
     </div>
 
 <?php include('views/elements/footer.php'); ?>
