9602bb5da3783d22b079daf872ceee102c7434f6
joaschwe <joaschwe@iupui.edu>
2018-04-09 01:34:36 -0400

    changed xml path to generated path from world weather online site

--- assign4/application/controllers/weathercontroller.php
+++ assign4/application/controllers/weathercontroller.php
@@ -2,7 +2,8 @@
 class WeatherController extends Controller{
 
 	public function index(){
-	    $xml = simplexml_load_file('api.worldweatheronline.com/free/v2/weather.ashx?q=46076&format=xml&num_of_days=2&key=7360a0596f8f4826b1845340180904');
+//	    $xml = simplexml_load_file('api.worldweatheronline.free/v2/weather.ashx?q=46076&format=xml&num_of_days=2&key=7360a0596f8f4826b1845340180904');
+	    $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q=46076&format=xml&num_of_days=2');
 	    $this->set(weather, $xml);
 
 	}
