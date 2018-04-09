a757dbe495364b98e378d582ab3eac6fd25c6b24
joaschwe <joaschwe@iupui.edu>
2018-04-09 02:00:04 -0400

    method getResults(); result true and false

--- assign4/application/controllers/weathercontroller.php
+++ assign4/application/controllers/weathercontroller.php
@@ -2,10 +2,14 @@
 class WeatherController extends Controller{
 
 	public function index() {
-	    $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q=46076&format=xml&num_of_days=2');
-	    $this->set(weather, $xml);
-
+	    $this->set(result,false);
 	}
+
+	public function getResults() {
+        $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q=46076&format=xml&num_of_days=2');
+        $this->set(result,true);
+        $this->set(weather, $xml);
+    }
 	
 }
 ?>
