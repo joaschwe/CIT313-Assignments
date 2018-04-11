92e4a0f7cbbfd7c2c5ac2ddd0474a87f16f76330
joaschwe <joaschwe@iupui.edu>
2018-04-10 23:30:16 -0400

    going back to orig weather api

--- assign4/views/weather/index.php
+++ assign4/views/weather/index.php
@@ -22,7 +22,7 @@ else {  ?>
 
     <div class="container">
         <div class="page-header">
-            <h1>Current Weather for <?php echo $weather->request->query; ?></h1>
+            <h1>Weather for <?php echo $weather->request->query; ?></h1>
         </div>
 
         <h4>Today's High: <?php echo $weather->weather->maxtempF; ?></h4>
