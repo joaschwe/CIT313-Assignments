d7d04fa5ba400edf7c7c21f48b2a868c2b439913
joaschwe <joaschwe@iupui.edu>
2018-04-11 01:38:20 -0400

    changed weather api back to wunderground

--- assign4/application/controllers/weathercontroller.php
+++ assign4/application/controllers/weathercontroller.php
@@ -8,7 +8,8 @@ class WeatherController extends Controller{
 	}
 
 	public function getResults() {
-        $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q='.$_POST["zip"].'&format=xml&num_of_days=2');
+//        $xml = simplexml_load_file('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=c915aee8414440959f752714180904&q='.$_POST["zip"].'&format=xml&num_of_days=2');
+        $xml = simplexml_load_file('http://api.wunderground.com/api/2510a1887ee3b519/forecast/geolookup/conditions/q/'.$_POST["zip"].'.xml');
         $this->set(result,true);
         $this->set(weather, $xml);
     }
--- assign4/views/weather/index.php
+++ assign4/views/weather/index.php
@@ -21,11 +21,13 @@ else {  ?>
 
     <div class="container">
         <div class="page-header">
-            <h1>Weather for <?php echo $weather->request->query; ?></h1>
+            <h1>Weather for <?php echo $weather->current_observation->display_location->full; ?></h1>
         </div>
 
-        <h4>Today's High: <?php echo $weather->weather->maxtempF; ?></h4>
-        <h4>Today's Low: <?php echo $weather->weather->mintempF; ?></h4>
+        <h4><?php echo $weather->current_observation->weather; ?>&nbsp;<img src="<?php echo $weather->current_observation->icon_url; ?>" /></h4>
+        <h4>Temperature: <?php echo $weather->current_observation->temperature_string; ?></h4>
+        <h4>Wind: <?php echo $weather->current_observation->wind_string; ?></h4>
+        <h4>Humidity: <?php echo $weather->current_observation->pressure_in . ' (' . $weather->current_observation->pressure_trend . ')'; ?></h4>
     </div>
 <?php
 }
