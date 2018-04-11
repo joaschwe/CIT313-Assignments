6401b5783cb934a41f03ec5b6f28f47920d6b644
joaschwe <joaschwe@iupui.edu>
2018-04-11 02:25:41 -0400

    moved scripts out of header and into footer

--- assign4/views/elements/header.php
+++ assign4/views/elements/header.php
@@ -99,37 +99,3 @@
         </div>
       </div>
     </div>
-
-    <?php
-    if( $u->isAdmin() ) { ?>
-
-    <!-- Le javascript
-================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-
-    <script>
-        var editor = new TINY.editor.edit('editor', {
-            id: 'tinyeditor',
-            width: 584,
-            height: 175,
-            cssclass: 'tinyeditor',
-            controlclass: 'tinyeditor-control',
-            rowclass: 'tinyeditor-header',
-            dividerclass: 'tinyeditor-divider',
-            controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
-                'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
-                'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
-                'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|'],
-            footer: true,
-            fonts: ['Verdana','Arial','Georgia','Trebuchet MS'],
-            xhtml: true,
-            cssfile: 'custom.css',
-            bodyid: 'editor',
-            footerclass: 'tinyeditor-footer',
-            toggle: {text: 'source', activetext: 'wysiwyg', cssclass: 'toggle'},
-            resize: {cssclass: 'resize'}
-        });
-
-
-    </script>
-<?php } ?>
