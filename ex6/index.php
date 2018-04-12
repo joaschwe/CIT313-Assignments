ab65dc0650bf074982c6c01bfeca8f1f2906e853
joaschwe <joaschwe@iupui.edu>
2018-04-12 03:31:07 -0400

    housekeeping

--- ex6/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// Display errors in production mode
-//ini_set('display_errors', E_ALL);
-ini_set('display_errors', 0);
-//ini_set('display_errors', 1);
-// let's get started
-require 'application/router.php';
-
