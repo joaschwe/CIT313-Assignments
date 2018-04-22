1e25ca436e00fedc58ca364a6072ec07af4d541b
Joanna Schweiger <joaschwe@iupui.edu>
2018-04-22 16:44:26 -0400

    added base for final-part1_v2 project

--- final-p1_v2/application/config.php
+++ final-p1_v2/application/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
-define('BASE_URL', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/final-p1/');
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/final-p1_v2/');
 
 //database info
 define('DB_HOST', 'localhost');
